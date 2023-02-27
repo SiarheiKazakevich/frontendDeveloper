2bd3baa7db674939ec38faa4f8d43ae7056c0c3e
SiarheiKazakevich <dianrom@tut.by>
2023-02-27 14:56:24 +0300

    feat: add time

--- main.js
+++ main.js
@@ -15,3 +15,33 @@ document.getElementById('target').addEventListener('click', function () {
       elem.classList.remove('anim')
    }, 500)
 }, false)
+
+
+/*-------1a) Текущее время -----*/
+
+const time = document.querySelector('.time');
+function showTime() {
+   return new Date().toLocaleTimeString();
+}
+setInterval(
+   () => time.textContent = showTime(), 1000
+);
+
+
+/*-----1b) выводим текущую дату-------*/
+
+const placeDate = document.querySelector('.date');
+function showFullDate() {
+   const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday',
+      'Thursday', 'Friday', 'Saturday'];
+   const months = ['Январь', 'February', 'March', 'April', 'May', 'June',
+      'July', 'August', 'September', 'November', 'December'];
+
+   const myDate = new Date();
+   const fullDate = days[myDate.getDay()] + ', ' +
+      months[myDate.getMonth()] + ' ' + myDate.getDate();
+   return fullDate;
+};
+setInterval(
+   () => placeDate.textContent = showFullDate(), 1000
+);
