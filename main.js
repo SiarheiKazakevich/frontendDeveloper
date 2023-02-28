daa6e7219b541ce117133b4a3b82d3beb0e29404
SiarheiKazakevich <dianrom@tut.by>
2023-02-28 09:13:12 +0300

    feat: add weather

--- main.js
+++ main.js
@@ -45,3 +45,20 @@ function showFullDate() {
 setInterval(
    () => placeDate.textContent = showFullDate(), 1000
 );
+
+
+/*---------------------погода------------------*/
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+const city = document.querySelector('.city');
+
+async function getWeather() {
+   const url = `https://api.openweathermap.org/data/2.5/weather?q=Минск&lang=ru&appid=5e07cc3fbd9370b8c188d3f04953c8ad&units=metric`;
+   const res = await fetch(url);
+   const data = await res.json();
+   console.log(data.weather[0].id, data.main.temp);
+   weatherIcon.className = 'weather-icon owf';
+   weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+   temperature.textContent = `${data.main.temp.toFixed(0)}°C`;
+}
+getWeather();
